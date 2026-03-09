1ad82216cacb4d6efcc7fb8c39d85455a5d9c3fa
Đặng Vĩnh Hưng <hungdvce170162@fpt.edu.vn>
2026-03-09 10:03:47 +0700

    HungDVCE170162_ChangePasswordUser_Add password validation

--- src/main/java/controller/ChangePasswordServlet.java
+++ src/main/java/controller/ChangePasswordServlet.java
@@ -7,12 +7,12 @@ package controller;
 import dao.AccountDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import model.Account;
 import model.Customer;
 
@@ -125,3 +125,4 @@ public class ChangePasswordServlet extends HttpServlet {
     }// </editor-fold>
 
 }
+
