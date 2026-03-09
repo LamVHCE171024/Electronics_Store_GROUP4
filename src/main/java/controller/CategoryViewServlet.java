4f1f57089ae634a8ad8bf997cb5215144c7662e0
LamVHCE171024 <lamvhce171024@fpt.edu.vn>
2026-03-09 12:33:00 +0700

    ThuongNVCE181966_CategoryViewServlet_Display products by category

--- src/main/java/controller/CategoryViewServlet.java
+++ src/main/java/controller/CategoryViewServlet.java
@@ -9,17 +9,17 @@ import dao.CategoryDAO;
 import dao.ProductDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import model.Category;
 
 /**
  *
- * @author HP - Gia Khiêm
+ * @author pc
  */
 @WebServlet(name="CategoryViewServlet", urlPatterns={"/CategoryView"})
 public class CategoryViewServlet extends HttpServlet {
@@ -97,3 +97,4 @@ public class CategoryViewServlet extends HttpServlet {
     }// </editor-fold>
 
 }
+
